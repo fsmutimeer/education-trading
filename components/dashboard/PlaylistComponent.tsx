1f18ed9c092312f0357f304ea247950d999a604d
fsmutimeer <ishpata@hotmail.com>
2023-07-15 03:13:24 +0500

    responsive set

--- components/dashboard/PlaylistComponent.tsx
+++ components/dashboard/PlaylistComponent.tsx
@@ -222,7 +222,7 @@ const PlaylistComponent: React.FC<PlaylistComponentProps> = ({
 
   return (
     <Container>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         <div className="flex col-span-2 flex-wrap">
           <div className="flex justify-between items-center w-[100%] flex-wrap">
             <div className="flex gap-8">
